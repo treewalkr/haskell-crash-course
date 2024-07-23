a8f7a3e56b06294a3f4e0ca66834a455ca1a7670
hexhops <suthep@pinroj.com>
2024-07-23 13:48:05 +0700

    Fix typo

--- HW01.hs
+++ HW01.hs
@@ -5,8 +5,8 @@ data Shape
   = Circle Double
   | Square Double
   | CircleInCircle Double Double
-  | CircleWithIncribeSquare Double
-  | IncursiveSquareCircle Double
+  | CircleWithIncribedSquare Double
+  | RecursiveSquareCircle Double
   deriving (Show)
 
 -- Function to calculate the area of a shape
@@ -14,8 +14,8 @@ areaOf :: Shape -> Double
 areaOf (Circle r) = areaOfCircle r
 areaOf (Square s) = areaOfSquare s
 areaOf (CircleInCircle r1 r2) = areaDiff (Circle r1) (Circle r2)
-areaOf (CircleWithIncribeSquare d) = areaDiff (Circle (0.5 * d)) (Square (sideOfInscribedSquare d))
-areaOf (IncursiveSquareCircle d) = areaOfIncursiveCirclesIncribedSquares d
+areaOf (CircleWithIncribedSquare d) = areaDiff (Circle (0.5 * d)) (Square (sideOfInscribedSquare d))
+areaOf (RecursiveSquareCircle d) = areaOfRecursiveCirclesIncribedSquares d
 
 -- Function to calculate the difference between the areas of two shapes
 areaDiff :: Shape -> Shape -> Double
@@ -38,19 +38,19 @@ areaDiffOfTwoCircles :: Double -> Double -> Double
 areaDiffOfTwoCircles r1 r2 = areaOf (CircleInCircle r1 r2)
 
 -- 4. Function to calculate the aeare diff between a circle and its inscribed square given the diameter of the circle d
-areaDiffOfCircleWithIncribeSquare :: Double -> Double
-areaDiffOfCircleWithIncribeSquare d = areaOf (CircleWithIncribeSquare d)
+areaDiffOfCircleWithIncribedSquare :: Double -> Double
+areaDiffOfCircleWithIncribedSquare d = areaOf (CircleWithIncribedSquare d)
 
 -- 5. Function to recursively calculate the sum of area differences of recursive structure of squares incribed in circles given the side of the first square (diameter of the first circle)
-areaOfIncursiveCirclesIncribedSquares :: Double -> Double
-areaOfIncursiveCirclesIncribedSquares side
+areaOfRecursiveCirclesIncribedSquares :: Double -> Double
+areaOfRecursiveCirclesIncribedSquares side
   | side <= 0.0001 = 0 -- base case
-  | otherwise = areaDiffOfCircleWithIncribeSquare side + areaOfIncursiveCirclesIncribedSquares (sideOfInscribedSquare side)
+  | otherwise = areaDiffOfCircleWithIncribedSquare side + areaOfRecursiveCirclesIncribedSquares (sideOfInscribedSquare side)
 
--- Example: areaOfIncursiveCirclesIncribedSquares 1.0  -- Output: 0.5707963225421324
+-- Example: areaOfRecursiveCirclesIncribedSquares 1.0  -- Output: 0.5707963225421324
 
--- 6. General function to calculate the areas of a list of shapes given their defining dimension and shape constructor
+-- 6. Function to calculate the areas of shapes given a shape constructor and a list of shape dimensions
 areasOfShapes :: (Double -> Shape) -> [Double] -> [Double]
 areasOfShapes shapeConstructor = map (areaOf . shapeConstructor)
 
--- Example: areasOfShapes IncursiveSquareCircle [1.0, 2.0, 3.0]  -- Output: [0.5707963225421324,2.283185302926822,5.1371669363697094]
+-- Example: areasOfShapes RecursiveSquareCircle [1.0, 2.0, 3.0]  -- Output: [0.5707963225421324,2.283185302926822,5.1371669363697094]
